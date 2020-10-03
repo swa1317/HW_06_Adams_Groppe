b23b9e6017ec7574e05be7beaef8990a4ac433d3
Samuel Adams <swa1317@rit.edu>
2020-10-03 15:25:30 -0400

    part1

--- HW06_part1_Changing_K_only.m
+++ HW06_part1_Changing_K_only.m
@@ -1,7 +1,10 @@
 function HW06_part1_Changing_K_only(im_in)
-
+    
+    image_rgb = imread(im_in);
+    
     for K = 5:5:255
-        indexed_image = rgb2ind(im_in, K, 'nodither');
-        imshow(indexed_image);
+        indexed_image = rgb2ind(image_rgb, K, 'nodither');
+        imshow(indexed_image), title(['K = ', num2str(K)]);
+        pause(1);
     end
 end
